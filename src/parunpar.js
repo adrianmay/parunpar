ad6b9bd53de0363176805fb5cc95ae55f09c8266
Adrian May <adrian.alexander.may@gmail.com>
2014-09-26 11:46:43 +0800

    comments

--- src/parunpar.js
+++ src/parunpar.js
@@ -1,24 +1,38 @@
 "use strict"
 
+//Functional basics...
 var typeOf = function(x) { return x===null ? 'null' : typeof x}
 var id = function(x) { return x;}
 var konst = function(k) { return function (x) {return k;}}
 var eq = function(a) { return function (b) { return a===b; }}
 var map = function (horse) { return function (cart) { return cart.map(horse); }}
+
+//Slightly less basic...
 var runSnd = function(fs) { return function (x) { return fs[1]!==undefined ? [fs[0], fs[1](x)] : [fs[0]]}}
-
-var string_pp    = function (dir) { return function (x)  { return x; }}
-var number_pp    = function (dir) { return function (x)  { return dir ? x.toString() : Number(x); }}
-var boolean_pp   = function (dir) { return function (x)  { return dir ? (x ? 't' : 'f') : (x==='t'); }}
-var null_pp      = function (dir) { return function (x)  { return dir ? '' : null; }}
-var undefined_pp = function (dir) { return function (x)  { return dir ? '' : undefined; }}
-
 var thru = function (dir) { return function (ps) { return function (xs) {  
   var res = [];
   for (var i=0; i<ps.length && i<xs.length; i++) res[i] = ps[i](dir)(xs[i]);
   return res;
 }}}
 
+//A parunpar is a function of boolean that returns an encoder or decoder for true and false respectively.
+//A valid one obeys the laws:
+//  pp(false)( pp(true)(x) ) === x
+//  pp(true)( pp(false)(x) ) === x
+
+//Fundamental parunpars...
+var string_pp    = function (dir) { return function (x)  { return x; }}
+var number_pp    = function (dir) { return function (x)  { return dir ? x.toString() : Number(x); }}
+var boolean_pp   = function (dir) { return function (x)  { return dir ? (x ? 't' : 'f') : (x==='t'); }}
+var null_pp      = function (dir) { return function (x)  { return dir ? '' : null; }}
+var undefined_pp = function (dir) { return function (x)  { return dir ? '' : undefined; }}
+
+//Compose two parunpars. p2 is closest to the encoded text.
+var pipe = function (p1) { return function (p2) { return function (dir) { return function (x) { 
+  return dir ? p2(dir)(p1(dir)(x)) : p1(dir)(p2(dir)(x)) ;
+}}}}
+
+//Chops a string into fields by column widths. If string is longer than sum of columns, the rest forms a final field.
 var chop = function(cols) { return function (s) {
   var res = [], i=0;
   for ( var i=0; i<cols.length && s.length; s=s.substring(cols[i++]) )
@@ -27,27 +41,33 @@ var chop = function(cols) { return function (s) {
   return res;
 }}
 
+//Maps a column list and a list of parunpars onto a parunpar that allocates a fixed width to each parunpar...
 var fixedWidth = function (cols) { return function (ps) { return function (dir) { return function (x) { 
   return dir ? thru(dir)(ps)(x).join('') : thru(dir)(ps)(chop(cols)(x)) ;
 }}}}
 
+//Maps a separator and a list of parunpars onto a parunpar that serialises each with the sep in between
+//Doesn't think about what happens if the sep occurs in the serialisation of any field.
 var sepBy = function (sep) { return function (ps) { return function (dir) { return function (x) { 
   return dir ? thru(dir)(ps)(x).join(sep) : thru(dir)(ps)(x.split(sep)) ;
 }}}}
 
+//Maps one parunpar onto one that uses esc to POST-escape occurences of sep.
+//Doesn't work if sep or esc are special characters in regex syntax, so certainly not /, \ or ^
+//Feel free to use borng alphanumeric characters for either.
 var escape = function (sep, esc) { return function (p) { return function (dir) { return function (x) { 
   return dir ? p(dir)(x).replace(RegExp(sep,'g'),sep+esc) : p(dir)(x.replace(RegExp(sep+esc,'g'), sep));
 }}}}
 
+//Like sepBy except that all fields are escaped to avoid conflict with the separator.
 var sepByEsc = function (sep, esc) { return function (ps) { return function (dir) { return function (x) { 
   return sepBy (dir ? sep : RegExp(sep+"(?!"+esc+")","g")) ( map (escape(sep,esc)) (ps) ) (dir) (x) 
 }}}}
 
+//You can use this for everything...
 var tuple = sepByEsc(';',':');
 
-var pipe = function (p1) { return function (p2) { return function (dir) { return function (x) { 
-  return dir ? p2(dir)(p1(dir)(x)) : p1(dir)(p2(dir)(x)) ;
-}}}}
+//Exercise for the reader: write something for arrays of unnknown length but homogeneous contents.
 
 module.exports = { typeOf:typeOf, id:id, konst:konst, eq:eq, map:map, runSnd:runSnd, 
   string_pp:string_pp, number_pp:number_pp, boolean_pp:boolean_pp, null_pp:null_pp, 

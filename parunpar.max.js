c4174b807f5cca2fa0b46f4d1eb92f31aa3572ad
Adrian May <adrian.alexander.may@gmail.com>
2014-09-28 21:08:55 +0800

    Try

--- parunpar.max.js
+++ parunpar.max.js
@@ -1,4 +1,11 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+//This is for efficiently serialising objects you know the contents of.
+
+//It's not a JSON replacement. It's for when you want to control exactly 
+//  how things are encoded, want to do that hierarchially, and want to 
+//  write the en- and decoding code near each other for each bit of the
+//  hierarchy.
+
 "use strict"
 
 //Functional basics...
